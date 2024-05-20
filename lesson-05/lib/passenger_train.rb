d9c01a229e2224399f4aa7701a8b319a64d6a8d2
Alexander Scherbinin <a.sherbinin@key-g.com>
2024-05-20 15:04:20 +0300

    lesson 05

---
+++ lesson-05/lib/passenger_train.rb
@@ -0,0 +1,10 @@
+require_relative 'train'
+require_relative 'passenger_carriage'
+
+class PassengerTrain < Train
+  private
+
+  def type_of_carriage?(carriage)
+    carriage.is_a?(PassengerCarriage)
+  end
+end
---
+++ lesson-05/lib/train.rb
@@ -0,0 +1,77 @@
+class Train
+    attr_reader :number, :carriages, :speed, :route, :current_station_index
+
+    def initialize(number)
+      @number = number
+      @carriages = []
+      @speed = 0
+      @route = nil
+      @current_station_index = nil
+    end
+
+    def add_carriage(carriage)
+      return unless type_of_carriage?(carriage)
+      return unless stopped?
+
+      @carriages << carriage
+    end
+
+    def remove_carriage(carriage)
+      return unless stopped?
+
+      @carriages.delete(carriage)
+    end
+
+    def speed_up(value)
+      @speed += value
+    end
+
+    def slow_down(value)
+      @speed -= value
+      @speed = 0 if @speed < 0
+    end
+
+    def stopped?
+      @speed.zero?
+    end
+
+    def assign_route(route)
+      @route = route
+      @current_station_index = 0
+      current_station.add_train(self)
+    end
+
+    def move_forward
+      return unless next_station
+
+      current_station.remove_train(self)
+      @current_station_index += 1
+      current_station.add_train(self)
+    end
+
+    def move_backward
+      return unless previous_station
+
+      current_station.remove_train(self)
+      @current_station_index -= 1
+      current_station.add_train(self)
+    end
+
+    def current_station
+      @route.stations[@current_station_index] if @route
+    end
+
+    def next_station
+      @route.stations[@current_station_index + 1] if @route && @current_station_index < @route.stations.size - 1
+    end
+
+    def previous_station
+      @route.stations[@current_station_index - 1] if @route && @current_station_index > 0
+    end
+
+    private
+
+    def type_of_carriage?(carriage)
+      raise NotImplementedError, 'Этот метод должен быть переопределен в подклассах'
+    end
+  end
