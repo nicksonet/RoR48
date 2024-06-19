44ce926cd1bfe7c018743375002f03285208dd5c
Alexander Scherbinin <a.sherbinin@key-g.com>
2024-06-19 18:04:25 +0300

    lesson-07

--- cargo/lib/passenger_carriage.rb
+++ cargo/lib/passenger_carriage.rb
@@ -1,4 +1,17 @@
 require_relative 'carriage'
+require_relative 'validation'
 
 class PassengerCarriage < Carriage
-end
+    include Validation
+  
+    COMPANY_NAME_FORMAT = /\A[a-zA-Z\s&'-]{1,30}\z/.freeze
+  
+    validate :company_name, :presence
+    validate :company_name, :format, with: COMPANY_NAME_FORMAT
+  
+    def initialize(company_name)
+      super()
+      @company_name = company_name
+      validate!
+    end
+  end
--- cargo/lib/station.rb
+++ cargo/lib/station.rb
@@ -1,15 +1,23 @@
 require_relative 'instance_counter'
+require_relative 'validation'
 
 class Station
   include InstanceCounter
+  include Validation
 
   @@stations = []
 
   attr_reader :name, :trains
 
+  STATION_NAME_FORMAT = /\A[a-zA-Z\s-]{1,30}\z/.freeze
+
+  validate :name, :presence
+  validate :name, :format, with: STATION_NAME_FORMAT
+
   def initialize(name)
     @name = name
     @trains = []
+    validate!
     @@stations << self
     register_instance
   end
